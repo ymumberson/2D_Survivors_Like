a03cf0e4b2d0a612b1339b2548e65881a153c6b0
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-08-16 16:45:44 +0100

    Setup placeholder pause menu.

---
+++ Assets/UI/Scripts/UIController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject inGameUI;
+    
+    private InputAction openMenu;
+    private bool _isPaused = false;
+    public bool IsPaused => _isPaused;
+
+    void Awake()
+    {
+        openMenu = InputSystem.actions.FindAction("OpenMenu");
+        pauseMenu.SetActive(false);
+        inGameUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        CheckOpenMenu();
+    }
+
+    public void CheckOpenMenu()
+    {
+        if (!openMenu.WasPressedThisFrame()) return;
+
+        if (_isPaused)
+            UnPauseGame();
+        else
+            PauseGame();
+    }
+
+    public void PauseGame()
+    {
+        if (_isPaused) return;
+
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        _isPaused = true;
+    }
+
+    public void UnPauseGame()
+    {
+        if (!_isPaused) return;
+
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        _isPaused = false;
+    }
+}
