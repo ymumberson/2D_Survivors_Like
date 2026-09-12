1f423de563be643507a0755829664c43d2303091
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-09-12 16:13:03 +0100

    Updated the debug UI to destroy itself if not in editor, and also changed the current weapon upgrades to be rare instead of legendary.

--- Assets/UI/Scripts/UITestingScript.cs
+++ Assets/UI/Scripts/UITestingScript.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class UITestingScript : MonoBehaviour
 {
+#if !UNITY_EDITOR
+    void Awake()
+    {
+        
+        Destroy(this.gameObject);
+        
+    }
+#endif
+
     public void SetTimeScale(int timeScale)
     {
         Time.timeScale = timeScale;
